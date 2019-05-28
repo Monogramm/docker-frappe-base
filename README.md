9980ac16e3506abbdb8ac7289e3ba663e0a72890
mathieu.brunot <mb.mathieu.brunot@gmail.com>
2019-05-28 16:04:00 +0200

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+
+[uri_license]: http://www.gnu.org/licenses/agpl.html
+[uri_license_image]: https://img.shields.io/badge/License-AGPL%20v3-blue.svg
+
+[![License: AGPL v3][uri_license_image]][uri_license]
+[![Build Status](https://travis-ci.org/Monogramm/docker-frappe-base.svg)](https://travis-ci.org/Monogramm/docker-frappe-base)
+[![Docker Automated buid](https://img.shields.io/docker/build/monogramm/docker-frappe-base.svg)](https://hub.docker.com/r/monogramm/docker-frappe-base/)
+[![Docker Pulls](https://img.shields.io/docker/pulls/monogramm/docker-frappe-base.svg)](https://hub.docker.com/r/monogramm/docker-frappe-base/)
+[![](https://images.microbadger.com/badges/version/monogramm/docker-frappe-base.svg)](https://microbadger.com/images/monogramm/docker-frappe-base)
+[![](https://images.microbadger.com/badges/image/monogramm/docker-frappe-base.svg)](https://microbadger.com/images/monogramm/docker-frappe-base)
+
+# Frappe base Docker container
+
+Base Docker image to dockerize Frappe applications.
+
+## What is Frappe ?
+
+Full-stack web application framework that uses Python and MariaDB on the server side and a tightly integrated client side library. Built for [ERPNext](https://erpnext.com/).
+
+> [frapp.io](https://frappe.io/)
+> [github frappe](https://github.com/frappe/frappe)
+
+## Supported tags
+
+https://hub.docker.com/r/monogramm/docker-frappe-base/
+
+* alpine
+    - `alpine-3.9` `alpine` `latest`
+    - `alpine-3.8`
+* debian
+    - `debian-stretch` `debian`
+    - `debian-stretch-slim`
+    - `debian-jessie`
+    - `debian-jessie-slim`
+
+# Questions / Issues
+If you got any questions or problems using the image, please visit our [Github Repository](https://github.com/Monogramm/docker-frappe-base) and write an issue.  
---
+++ update.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+set -eo pipefail
+
+declare -A versions=(
+	[debian]='stretch stretch-slim jessie jessie-slim'
+	[alpine]='3.8 3.9'
+)
+
+declare -A base=(
+	[debian]='debian'
+	[alpine]='alpine'
+)
+
+variants=(
+	debian
+	alpine
+)
+
+
+# version_greater_or_equal A B returns whether A >= B
+function version_greater_or_equal() {
+	[[ "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1" || "$1" == "$2" ]];
+}
+
+dockerRepo="monogramm/docker-frappe-base"
+
+# Remove existing images
+echo "reset docker images"
+rm -rf ./images/
+mkdir -p ./images/
+
+echo "update docker images"
+travisEnv=
+for variant in "${variants[@]}"; do
+	IFS=', ' read -r -a varVersions <<< "${versions[$variant]}"
+
+	for version in "${varVersions[@]}"; do
+		# Create the variant directory with a Dockerfile.
+		dir="images/$variant/$version"
+		if [ -d "$dir" ]; then
+			continue
+		fi
+		echo "generating $variant [$version]"
+		mkdir -p "$dir"
+
+		template="Dockerfile-${base[$variant]}.template"
+		cp "$template" "$dir/Dockerfile"
+
+		# Replace the variables.
+		sed -ri -e '
+			s/%%VARIANT%%/'"$variant"'/g;
+			s/%%VERSION%%/'"$version"'/g;
+		' "$dir/Dockerfile"
+
+		# Copy the shell scripts
+		#for name in entrypoint; do
+		#	cp "docker-$name.sh" "$dir/$name.sh"
+		#	chmod 755 "$dir/$name.sh"
+		#done
+
+		cp ".dockerignore" "$dir/.dockerignore"
+
+		travisEnv='\n    - VERSION='"$version"' VARIANT='"$variant$travisEnv"
+
+		if [[ $1 == 'build' ]]; then
+			tag="$variant-$version"
+			echo "Build Dockerfile for ${tag}"
+			docker build -t ${dockerRepo}:${tag} $dir
+		fi
+	done
+
+done
+
+# update .travis.yml
+travis="$(awk -v 'RS=\n\n' '$1 == "env:" && $2 == "#" && $3 == "Environments" { $0 = "env: # Environments'"$travisEnv"'" } { printf "%s%s", $0, RS }' .travis.yml)"
+echo "$travis" > .travis.yml
